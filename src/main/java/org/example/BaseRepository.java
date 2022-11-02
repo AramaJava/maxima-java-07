afdcf9a63b2d59aaef4e53f1d6a3cf72ac5ff8c7
AramaJava <abr-aram@yandex.ru>
2022-11-03 04:05:34 +0500

    next

--- src/main/java/org/example/BaseRepository.java
+++ src/main/java/org/example/BaseRepository.java
@@ -1,13 +1,11 @@
 package org.example;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BaseRepository <T> {
 
-    List<T> read(int id);
-
     void create(T t);
+    List<T> read(int id);
 
     void update(T t, String[] params);
 
@@ -17,10 +15,6 @@ public interface BaseRepository <T> {
 
 }
 
-
-
-  //  С Optional и обобщениями - хорошие идеи.
-//  Но тогда уж List> :))
 //  В read и delete нужно заменить T t на какой-то идентификатор объекта,
 //  иначе придется сравнивать все объекты в базе с указанным.
 //  В update этот идентификатор нужно добавить.
